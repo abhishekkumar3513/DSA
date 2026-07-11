a16ec9b294268081d33f96d3436d85f0a764ea76
Abhishek Kumar <167641857+abhishekkumar3513@users.noreply.github.com>
2026-07-11 19:53:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (39.61%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -4,7 +4,7 @@ class Solution {
         int high=arr.length-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(arr[mid]-mid-1<k){
+            if(arr[mid]-mid-1< k){
                 low=mid+1;
             }
             else{
