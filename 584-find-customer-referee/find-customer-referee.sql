a37e33511a98614b8ee36d86bfec9175d7663d6e
Abhishek Kumar <167641857+abhishekkumar3513@users.noreply.github.com>
2026-08-29 00:46:35 +0530

    Time: 568 ms (34.69%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,2 +1,2 @@
--- Write your PostgreSQL query statement below
-SELECT name from Customer where referee_id IS NULL OR referee_id!=2;
+# Write your MySQL query statement below
+select name from Customer where referee_id!=2 OR referee_id IS null;
